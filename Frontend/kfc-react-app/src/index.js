886fe36d192042f8efb5898c4e1ae3dc024d9438
suraj sanjay kokane <108039887+kokanesuraj007@users.noreply.github.com>
2022-12-15 11:35:42 +0530

    fw20_0195_day-2

--- Frontend/kfc-react-app/src/index.js
+++ Frontend/kfc-react-app/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import  {Provider}  from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Provider>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </Provider>
   </React.StrictMode>
 );
 
