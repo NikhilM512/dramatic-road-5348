834bbe3b44c80b6216d896161c62cf47de700bcf
suraj sanjay kokane <108039887+kokanesuraj007@users.noreply.github.com>
2022-12-16 20:52:18 +0530

    fw20_0195

---
+++ Frontend/kfc-react-app/src/Components/main_button/Button.jsx
@@ -0,0 +1,21 @@
+
+// import styled  from "styled-components"
+
+import styled from "@emotion/styled";
+
+export const Button = styled.button`
+    background-color: rgb(228,0,43);
+    padding: 11px 40px;
+    border-radius: 200px;
+    height: 44px;
+    text-align: center;
+    align-items: center;
+    flex-direction: row;
+    font-family: "National 2 Regular";
+    font-style: normal;
+    font-size: 14px;
+    line-height: 24px;
+    cursor: pointer;
+    color: white;
+    border: none;
+`
